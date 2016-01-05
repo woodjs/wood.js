9e5fb8c818fe28ba15a1a6ff396159d1afd5cea3
yanlv.yin <yanlv.yin@servision.com.cn>
2016-01-05 18:05:00 +0800

    begin wood.js

---
+++ wood.js
@@ -0,0 +1,30 @@
+"use strict";
+
+(function () {
+  var version = '0.0.1';
+  var wood = {};
+  var util = {
+    isFunction: function () {
+
+    },
+    isArray: function () {
+
+    },
+    isObject: function () {
+
+    }
+  };
+  wood.prototype = {
+    /**
+     * 深拷贝
+     */
+    deepCopy: function () {
+      let target = arguments[0] || {};
+      let i = 0;
+
+      
+    }
+  };
+
+  window.wood = wood;
+}());
